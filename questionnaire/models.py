f74e25239736a19d43a887e0f472290d74ab1220
shapran_roman <romanshapran96@gmail.com>
2020-11-13 03:00:05 +0300

    add Meta classes and __str__ to models

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -15,18 +15,35 @@ class Test(models.Model):
     def get_absolute_url(self):
         return reverse("questionnaire:test-detail", args=[self.pk])
 
+    def __str__(self):
+        return self.name
+
 
 class ExtendedUser(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     tests = models.ManyToManyField(Test, verbose_name="Тесты")
+
+    class Meta:
+        verbose_name = "Расширенный пользователь"
+        verbose_name_plural = "Расширенные пользователи"
+
+    def __str__(self):
+        return self.user.username
 
 
 class Question(models.Model):
     test = models.ManyToManyField(Test, verbose_name="Тесты")
     text = models.TextField(max_length=500, verbose_name="Вопрос")
 
+    class Meta:
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
+
     def get_absolute_url(self):
         return reverse("questionnaire:question-detail", args=[self.pk])
+
+    def __str__(self):
+        return self.text
 
 
 class PassedTest(models.Model):
@@ -36,11 +53,25 @@ class PassedTest(models.Model):
     is_done = models.BooleanField(default=False)
     last_question = models.ForeignKey(Question, models.CASCADE, null=True)
 
+    class Meta:
+        verbose_name = "Пройденный тест"
+        verbose_name_plural = "Пройденные тесты"
+
+    def __str__(self):
+        return f"{self.user.user.username}: {self.test.name}"
+
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="Вопрос")
     text = models.TextField(max_length=200, verbose_name="Вариант ответа")
     is_correct = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Ответ"
+        verbose_name_plural = "Ответы"
+
+    def __str__(self):
+        return self.text
 
 
 class PassedQuestion(models.Model):
@@ -54,3 +85,9 @@ class PassedQuestion(models.Model):
     test = models.ForeignKey(Test, models.CASCADE)
     question = models.ForeignKey(Question, models.CASCADE)
 
+    class Meta:
+        verbose_name = "Пройденный вопрос"
+        verbose_name_plural = "Пройденные вопросы"
+
+    def __str__(self):
+        return f"{self.user.user.username}: {self.test}"
