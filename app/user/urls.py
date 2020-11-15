e5637f3c4460fdd3c3c01e134acef85739072cff
shapran_roman <romanshapran96@gmail.com>
2020-11-15 17:09:40 +0300

    add user account page

--- app/user/urls.py
+++ app/user/urls.py
@@ -7,8 +7,8 @@ from . import views
 app_name = "user"
 
 urlpatterns = [
-    # не думаю что в рамках тестового задания нужно что то переопределять в этих классах
     path("sign_in/", LoginView.as_view(template_name="user/login.html"), name="sign_in"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("sign_up/", views.RegistrationFormView.as_view(), name="sign_up")
+    path("sign_up/", views.RegistrationFormView.as_view(), name="sign_up"),
+    path("account/", views.AccountView.as_view(), name="account")
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+
 from django.urls import reverse
+
 from django.views.generic import FormView
+from django.views import View
+
+from django.contrib.auth.models import User
+
+from questionnaire.models import ExtendedUser, PassedTest
 
 from . import forms
 # Create your views here.
@@ -17,3 +24,17 @@ class RegistrationFormView(FormView):
         self.success_url = reverse("user:sign_in")
         form.save()
         return super().form_valid(form)
+
+
+class AccountView(View):
+    def get_context_data(self, *args, **kwargs):
+        user = self.request.user
+        extended_user = ExtendedUser.objects.get(user_id=user.pk)
+        passed_tests = PassedTest.objects.filter(user=extended_user)
+        return {
+            "user": user,
+            "passed_tests": passed_tests
+        }
+
+    def get(self, request, *args, **kwargs):
+        return render(request, "user/account.html", self.get_context_data(*args, **kwargs))
